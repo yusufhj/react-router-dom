e77fbf1fccdfdafbbb3d2e00ba50981b2fe1941c
Yusuf AlHamad <ylive71@gmail.com>
2024-11-07 13:10:42 +0300

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+// src/App.jsx
+import { useState } from 'react';
+import PokemonList from './components/PokemonList';
+
+const initialState = [
+  { _id: 1, name: 'bulbasaur', weight: 69, height: 7 },
+  { _id: 2, name: 'ivysaur', weight: 130, height: 10 },
+  { _id: 3, name: 'venusaur', weight: 1000, height: 20 },
+  { _id: 4, name: 'charmander', weight: 85, height: 6 },
+  { _id: 5, name: 'charmeleon', weight: 190, height: 11 },
+];
+
+const App = () => {
+  const [pokemon, setPokemon] = useState(initialState);
+  return (
+    <>
+      <h1>Pokemon!</h1>
+      <PokemonList pokemon={pokemon} />
+    </>
+  );
+};
+
+export default App;
